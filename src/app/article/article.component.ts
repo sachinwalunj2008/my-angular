509e3cbdc91c15ddd17b272996db48506d7681d9
Sachin Walunj <sachinwalunj2008@gmail.com>
2020-08-22 20:10:52 +0530

    ds

---
+++ src/app/article/article.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, HostBinding } from '@angular/core';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.scss']
+})
+export class ArticleComponent implements OnInit {
+  @HostBinding('attr.class') cssClass = 'row';
+  votes: number;
+  title: string;
+  link: string;
+
+  constructor() {
+    this.title = 'Angular 2';
+    this.link = 'www.google.com';
+    this.votes = 10;
+  }
+  voteUp() {
+    this.votes += 1;
+  }
+  voteDown(): boolean {
+    this.votes -= 1;
+    return false;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
